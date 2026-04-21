a16486fc46f724d20a0aa281f7f1660e9a950c2b
Ccaparas666 <carcapz123@gmaik.com>
2026-04-22 05:49:22 +0800

    ADD BUTTON FUNCTION

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,10 +1,12 @@
 import dayjs from "dayjs";
-import { Edit, Plus, Trash } from "lucide-react";
+import { Edit, Plus, Trash, Save } from "lucide-react";
 import { useState } from "react";
 
 export const Projects = () => {
   const [inputTodoList, setinputTodoList] = useState("");
   const [todoList, setTodoList] = useState([]);
+  const [editingId, setEditingId] = useState(null);
+  const [editValue, setEditValue] = useState("");
 
   function saveInputText(event) {
     setinputTodoList(event.target.value);
@@ -29,6 +31,24 @@ export const Projects = () => {
 
   function deleteTodo(id) {
     setTodoList((prev) => prev.filter((item) => item.id !== id));
+  }
+
+  function editTodo(id, currentValue) {
+    setEditingId(id);
+    setEditValue(currentValue || "");
+  }
+
+  function saveEdit(id) {
+    setTodoList((prev) =>
+      prev.map(
+        (item) =>
+          item.id === id
+            ? { ...item, todo: editValue } // update this one
+            : item, // keep others unchanged
+      ),
+    );
+
+    setEditingId(null);
   }
 
   return (
@@ -68,17 +88,44 @@ export const Projects = () => {
           <tbody>
             {todoList.map((item) => (
               <tr key={item.id} className="border-t">
-                <td className="px-4 py-2 max-w-xs wrap-break-word">
-                  {item.todo}
+                <td className="px-4 py-2 max-w-xs break-words">
+                  {editingId === item.id ? (
+                    <input
+                      type="text"
+                      value={editValue}
+                      onChange={(e) => setEditValue(e.target.value)}
+                      onBlur={() => saveEdit(item.id)}
+                      className="w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                  ) : (
+                    item.todo
+                  )}
                 </td>
+
                 <td className="px-4 py-2">{item.date}</td>
                 <td className="px-4 py-2 text-center">
-                  <Edit className="inline-block cursor-pointer" />
+                  {editingId === item.id ? (
+                    <Save
+                      color="#23ec09"
+                      strokeWidth={3}
+                      className="inline-block cursor-pointer"
+                      onClick={() => saveEdit(item.id)}
+                    />
+                  ) : (
+                    <Edit
+                      strokeWidth={3}
+                      color="skyblue"
+                      className="inline-block cursor-pointer"
+                      onClick={() => editTodo(item.id)}
+                    />
+                  )}
                 </td>
                 <td className="px-4 py-2 text-center">
                   <Trash
+                    strokeWidth={3}
+                    color="red"
                     className="inline-block cursor-pointer"
-                    onClick={() => deleteTodo(item.id)}
+                    onClick={() => deleteTodo(item.id, item.todo)}
                   />
                 </td>
               </tr>
